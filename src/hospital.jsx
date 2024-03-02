93150714fd070ae8e74e87f0d77bf765e13a19ae
Maarij Bukhari <syedmuhammadmaarij@gmail.com>
2024-03-02 19:31:54 +0500

    Initial commit

---
+++ src/hospital.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { doctorDetail, patientDetail } from "./hospital";
+
+export default function Hospital() {
+    function handleFormSubmit(event) {
+        event.preventDefault();
+    }
+
+    return (
+        <div className='h-screen flex flex-col justify-center items-center bg-gray-900 text-white'>
+            <h1 className='text-4xl font-bold mb-8'>HOSPITAL MANAGEMENT SYSTEM</h1>
+            <form className='flex flex-col gap-4' onSubmit={handleFormSubmit}>
+                <div className="">
+                    {
+                        doctorDetail.map((doctor, index) => (
+                            <div key={index} className="">
+                                <label htmlFor={`doctorField-${index}`} className='text-xl font-bold mb-2'>{doctor.title}</label>
+                                <input id={`doctorField-${index}`} type='text' inputMode='numeric' pattern='[0-9]*' className='px-4 py-2 rounded-md bg-gray-800 text-white' placeholder={doctor.placeholder} />
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="">
+                    {
+                        patientDetail.map((patient, index) => (
+                            <div key={index} className="">
+                                <label htmlFor={`patientField-${index}`} className='text-xl font-bold mb-2'>{patient.title}</label>
+                                <input id={`patientField-${index}`} type='text' inputMode='numeric' pattern='[0-9]*' className='px-4 py-2 rounded-md bg-gray-800 text-white' placeholder={patient.placeholder} />
+                            </div>
+                        ))
+                    }
+                </div>
+                <button type="submit" className='px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300'>Submit</button>
+            </form>
+        </div>
+    );
+}
