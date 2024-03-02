2305f22fd33c3051e82209891f9911e0b1b5d445
Gano603 <ganogaming603@gmail.com>
2024-03-02 19:49:20 +0500

    Project Initiliaze

---
+++ src/HospitalData.js
@@ -0,0 +1,69 @@
+export const doctorDetail=[
+    {
+        title:"DoctorID",
+        placeholder:"DoctorID",
+    },
+    {
+        title:"Doctor Title",
+        placeholder:"Doctor's title"
+    },
+    {
+        title:"Doctor Last Name",
+        placeholder:"Doctor's Last Name"
+    },
+    {
+        title:"Doctor First Name",
+        placeholder:"Doctor's first name"
+    },
+    {
+        title:"Tel-No.",
+        placeholder:"000 0000 0000"
+    },
+    {
+        title:"Appointment",
+        placeholder:"Appointment Id"
+    },
+    {
+        title:"Dates",
+        placeholder:"Dates"
+    },
+    {
+        title:"Time",
+        placeholder:"Doctor's title"
+    },
+]
+export const patientDetail=[
+    {
+        title:"PatientID",
+        placeholder:"PatientID",
+    },
+    {
+        title:"Patient Title",
+        placeholder:"Patient's title"
+    },
+    {
+        title:"Patient Last Name",
+        placeholder:"Patient's Last Name"
+    },
+    {
+        title:"Patient First Name",
+        placeholder:"Patient's first name"
+    },
+    {
+        title:"AddressLine1",
+        placeholder:"Address line 1"
+    },
+    {
+        title:"PostCode",
+        placeholder:"postCode"
+    },
+    {
+        title:"Age",
+        placeholder:"age"
+    },
+    {
+        title:"Male",
+        placeholder:"Male"
+    },
+]
+// export {doctorDetail,patientDetail};
